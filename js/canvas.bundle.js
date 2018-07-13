3b62f2e00228e907d76286a91fc8d2179311ee0d
noahub <noah.matsell@unbounce.com>
2018-07-13 16:00:50 -0700

    Initial dist subtree commit

---
+++ js/canvas.bundle.js
@@ -0,0 +1,251 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/canvas.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/canvas.js":
+/*!***********************!*\
+  !*** ./src/canvas.js ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _utils = __webpack_require__(/*! ./utils */ "./src/utils.js");
+
+var _utils2 = _interopRequireDefault(_utils);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+var gravity = 1;
+var friction = 0.8;
+var colors = ['#FD4A4C', '#0098db', '#E5E4DB', '#2d2d2d'];
+
+var gravitySlider = document.getElementById('gravity');
+var frictionSlider = document.getElementById('bounciness');
+var button = document.querySelector('button');
+
+canvas.width = window.innerWidth * 0.98;
+canvas.height = window.innerHeight * 0.98;
+
+var mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2
+};
+
+// Event Listeners
+addEventListener('mousemove', function (event) {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+
+addEventListener('resize', function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+});
+
+button.addEventListener('click', function (event) {
+  init();
+});
+
+gravitySlider.addEventListener('change', function (e) {
+  gravity = e.target.value / 10;
+  init();
+});
+frictionSlider.addEventListener('change', function (e) {
+  friction = e.target.value / 10;
+  init();
+});
+// Objects
+function Ball(x, y, dx, dy, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.color = color;
+}
+
+Ball.prototype.update = function () {
+  if (this.y + this.radius + this.dy > canvas.height) {
+    this.dy = -this.dy * friction;
+  } else {
+    this.dy += gravity;
+  }
+
+  if (this.x + this.radius + this.dx > canvas.width || this.x - this.radius <= 0) {
+    this.dx = -this.dx;
+  }
+
+  this.x += this.dx;
+  this.y += this.dy;
+  this.draw();
+};
+
+Ball.prototype.draw = function () {
+  c.beginPath();
+  c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+  c.fillStyle = this.color;
+  c.fill();
+  c.stroke();
+  c.closePath();
+};
+
+// Implementation
+var objects = void 0;
+// let ball = new Ball(canvas.width / 2, canvas.height / 2, 0, 2, 30, 'red');
+
+function init() {
+  objects = [];
+  gravitySlider.value = gravity * 10;
+  frictionSlider.value = friction * 10;
+  document.querySelector('#gravitylabel span').innerText = gravity;
+  document.querySelector('#bouncinesslabel span').innerText = friction * 10;
+
+  for (var i = 0; i < 700; i++) {
+    var radius = _utils2.default.randomIntFromRange(8, 20);
+    var x = _utils2.default.randomIntFromRange(radius, canvas.width - radius);
+    var y = _utils2.default.randomIntFromRange(0, canvas.height - radius);
+    var dx = _utils2.default.randomIntFromRange(-2, 2);
+    var dy = _utils2.default.randomIntFromRange(-2, 2);
+    var color = _utils2.default.randomColor(colors);
+    objects.push(new Ball(x, y, dx, dy, radius, color));
+  }
+}
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  // ball.update();
+
+  objects.forEach(function (object) {
+    object.update();
+  });
+}
+
+init();
+animate();
+
+/***/ }),
+
+/***/ "./src/utils.js":
+/*!**********************!*\
+  !*** ./src/utils.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function distance(x1, y1, x2, y2) {
+    var xDist = x2 - x1;
+    var yDist = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+}
+
+module.exports = { randomIntFromRange: randomIntFromRange, randomColor: randomColor, distance: distance };
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=canvas.bundle.js.map
